21a5bfeb1d7ec1bde9b8eb6874432c6d315a7fdb
Akvgatr <144269819+Akvgatr@users.noreply.github.com>
2025-01-31 16:34:39 +0530

    second commit

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.Whitenoisemiddleware'
 ]
 
 ROOT_URLCONF = 'studentstudyportal.urls'
